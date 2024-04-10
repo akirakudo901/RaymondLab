a750e543ad226872a86a0b3e938a0985be626e7a
AkiraKudo <akirakudo901@gmail.com>
2024-04-10 16:24:51 -0700

    Added specification to extract_frame_by_number and made it agnostic to input image extension.

--- DeepLabCut/video_related/extract_image_from_mp4.py
+++ DeepLabCut/video_related/extract_image_from_mp4.py
@@ -1,18 +1,34 @@
 # Author: Akira Kudo
 # Created: 2024/04/07
-# Last Updated: 2024/04/07
+# Last Updated: 2024/04/10
 
 import os
+from typing import List
 
 import ffmpy
 
-def extract_frame_by_number(input_file, output_file, frame_numbers : list):
+def extract_frame_by_number(input_file : str, 
+                            output_file : str, 
+                            frame_numbers : List[int]):
+    """
+    Extracts from an mp4 input_file, the frames specified by frame_numbers, and
+    save them under the name of output_file with a number appended at the end
+    indicating which frame the output file corresponds to.
+
+    :param str input_file: The path to the input mp4 video.
+    :param str output_file: The name every extracted frames will have (including
+    its extension), to which we append its frame number. e.g. OUTPUT3.jpg
+    :param List[int] frame_numbers: A list of integer indicating which frame to 
+    extract.
+    """
     # specify selected frames
     select_str = "select='"
     select_str += '+'.join(['eq(n\,{})'.format(fr_num) for fr_num in frame_numbers])
     select_str += "'"
     # output file is named by the nth output from the pipeline
-    nth_output_file = output_file.replace('.png', r'%d.png')
+    img_extension = os.path.splitext(output_file)[-1]
+    nth_output_file = output_file.replace('{}'.format(img_extension), 
+                                          r'%d{}'.format(img_extension))
     
     # Create FFmpeg command to extract frame by frame number
     ff = ffmpy.FFmpeg(
@@ -20,15 +36,15 @@ def extract_frame_by_number(input_file, output_file, frame_numbers : list):
         outputs={nth_output_file : f'-vf "{select_str}" -vsync vfr'}
     )
     
-    print(ff.cmd)
-    
     # Execute the FFmpeg command
     ff.run()
 
     # rename the resulting frames by their position in the video
     for img_idx in range(1, len(frame_numbers) + 1):
-        img_idx_th_output_file = output_file.replace('.png', f'{img_idx}.png')
-        frame_number_output_file = output_file.replace('.png', f'{frame_numbers[img_idx - 1]}.png')
+        img_idx_th_output_file = output_file.replace(f'{img_extension}', 
+                                                     f'{img_idx}{img_extension}')
+        frame_number_output_file = output_file.replace(f'{img_extension}', 
+                                                       f'{frame_numbers[img_idx - 1]}{img_extension}')
         os.rename(img_idx_th_output_file, frame_number_output_file)
 
 if __name__ == "__main__":
