c1d45115cea632ea15544838c544bb0e69310178
AkiraKudo <akirakudo901@gmail.com>
2024-04-12 19:01:10 -0700

    Cleaned up comment for extract_image_from_mp4

--- DeepLabCut/video_related/extract_image_from_mp4.py
+++ DeepLabCut/video_related/extract_image_from_mp4.py
@@ -1,6 +1,6 @@
 # Author: Akira Kudo
 # Created: 2024/04/07
-# Last Updated: 2024/04/10
+# Last Updated: 2024/04/12
 
 import itertools
 import os
@@ -65,47 +65,22 @@ def extract_frame_by_number(input_file : str,
             else:
                 os.rename(temp_idx_th_output_file, frame_number_output_file)
 
-    # TODO OLD IMPLEMENTATIOn
-    # # specify selected frames
-    # select_str = "select='"
-    # select_str += '+'.join(['eq(n\,{})'.format(fr_num) for fr_num in frame_numbers])
-    # select_str += "'"
-    # # output file is named by the nth output from the pipeline
-    # img_extension = os.path.splitext(output_file)[-1]
-    # nth_output_file = output_file.replace('{}'.format(img_extension), 
-    #                                       r'TEMP%d{}'.format(img_extension))
-    
-    # # Create FFmpeg command to extract frame by frame number
-    # ff = ffmpy.FFmpeg(
-    #     inputs={input_file : None},
-    #     # we wanna add -frames:v so that the 
-    #     outputs={nth_output_file : f'-vf "{select_str}" -vsync vfr -frames:v {len(frame_numbers)}'}
-    # )
-    
-    # # Execute the FFmpeg command
-    # ff.run()
-
-    # # rename the resulting frames by their position in the video
-    # for img_idx in range(1, len(frame_numbers) + 1):
-    #     img_idx_th_output_file = output_file.replace(f'{img_extension}', 
-    #                                                  f'TEMP{img_idx}{img_extension}')
-    #     frame_number_output_file = output_file.replace(f'{img_extension}', 
-    #                                                    f'{frame_numbers[img_idx - 1]}{img_extension}')
-    #     os.rename(img_idx_th_output_file, frame_number_output_file)
-
 if __name__ == "__main__":
     # Specify input MP4 file, output PNG file, and frame number of the frame to extract
-    input_folder = '/media/Data/Raymond Lab/Q175-D2Cre Open Field Males/Q175-D2Cre Open Field Males Brown-Judy-2024-01-12/videos'
-    input_name = '20220228231804_320151_m2_openfieldDLC_resnet50_Q175-D2Cre Open Field Males BrownJan12shuffle1_1030000_filtered_labeled.mp4'
+    # input_folder = '/media/Data/Raymond Lab/Q175-D2Cre Open Field Males/Q175-D2Cre Open Field Males Brown-Judy-2024-01-12/videos'
+    # input_name = '20220228231804_320151_m2_openfieldDLC_resnet50_Q175-D2Cre Open Field Males BrownJan12shuffle1_1030000_filtered_labeled.mp4'
     
-    output_folder = '/media/Data/Raymond Lab/to_delete_Python_Scripts/video_related/data/extracted_frames'
-    output_name = 'extracted.png'
+    # output_folder = '/media/Data/Raymond Lab/to_delete_Python_Scripts/video_related/data/extracted_frames'
+    # output_name = 'extracted.png'    
     
+    # input_file = os.path.join(input_folder, input_name)
+    # output_file = os.path.join(output_folder, output_name)
     
-    input_file = os.path.join(input_folder, input_name)
-    output_file = os.path.join(output_folder, output_name)
+    # frame_numbers = list(range(50, 61))
     
-    frame_numbers = list(range(50, 61))
-    
-    # Extract the frame
-    extract_frame_by_number(input_file, output_file, frame_numbers)
+    # # Extract the frame
+    # extract_frame_by_number(input_file, output_file, frame_numbers)
+
+    inputfile = r"Z:\Raymond Lab\2 Colour D1 D2 Photometry Project\Akira\DLC_data\Q175\videos\generated\320151m1_0to1000_20fps.mp4"
+    outputfile= r"Z:\Raymond Lab\2 Colour D1 D2 Photometry Project\Akira\DLC_data\Q175\videos\labeled_extracted\extracted.jpg"
+    extract_frame_by_number(inputfile, outputfile, list(range(1000)))
