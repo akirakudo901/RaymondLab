0755b7b6c259c76ef2b6eee3f8087de6c870ddfb
AkiraKudo <akirakudo901@gmail.com>
2024-04-17 19:46:55 -0700

    Added scaling factors to extract_image_from_mp4.

--- BSOID/label_behavior_bits/extract_image_from_mp4.py
+++ BSOID/label_behavior_bits/extract_image_from_mp4.py
@@ -12,17 +12,21 @@ FPS_FOR_FRAMENUMBER_TO_SECONDS_CONVERSION = 40
 
 def extract_frame_by_number(input_file : str, 
                             output_file : str, 
-                            frame_numbers : List[int]):
+                            frame_numbers : List[int],
+                            scaling_factor : float=1.0):
     """
     Extracts from an mp4 input_file, the frames specified by frame_numbers, and
     save them under the name of output_file with a number appended at the end
     indicating which frame the output file corresponds to.
+    Has option to scale the resulting figure by scaling_factor.
 
     :param str input_file: The path to the input mp4 video.
     :param str output_file: The name every extracted frames will have (including
     its extension), to which we append its frame number. e.g. OUTPUT3.jpg
     :param List[int] frame_numbers: A list of integer indicating which frame to 
     extract.
+    :param float scaling_factor: A factor to scale the output images by. 
+    Defaults to no scaling.
     """
     # below code thanks to: 
     # https://stackoverflow.com/questions/4628333/converting-a-list-of-integers-into-range-in-python/4629241#4629241
@@ -46,8 +50,9 @@ def extract_frame_by_number(input_file : str,
         # Create FFmpeg command to extract frame by frame number
         ff = ffmpy.FFmpeg(
             inputs={input_file : f' -ss {frame2seconds(start_frame)}'},
-            # we wanna add -frames:v so that the 
-            outputs={nth_output_file : f'-vsync vfr -frames:v {end_frame-start_frame+1}'}
+            # we wanna add -frames:v so that we only extract a set number of frames
+            outputs={nth_output_file : f'-vsync vfr -frames:v {end_frame-start_frame+1} ' + 
+                                       f' -vf scale="iw*{scaling_factor}:ih*{scaling_factor}"'}
         )
         
         # Execute the FFmpeg command
@@ -69,4 +74,4 @@ if __name__ == "__main__":
     # Specify input MP4 file, output PNG file, and frame number of the frame to extract
     inputfile = r"Z:\Raymond Lab\2 Colour D1 D2 Photometry Project\Akira\DLC_data\Q175\videos\generated\320151m1_0to1000_20fps.mp4"
     outputfile= r"Z:\Raymond Lab\2 Colour D1 D2 Photometry Project\Akira\DLC_data\Q175\videos\labeled_extracted\extracted.jpg"
-    extract_frame_by_number(inputfile, outputfile, list(range(1000)))
+    extract_frame_by_number(inputfile, outputfile, list(range(10)), scaling_factor=1.0)
