c3b47e87992fcf249e511683015d43c5da653428
AkiraKudo <akirakudo901@gmail.com>
2024-04-20 10:47:52 -0700

    Cleaned up code for scale_folder_content_by_n.

--- DeepLabCut/temper_with_csv_and_hdf/scale_csv_by_n.py
+++ DeepLabCut/temper_with_csv_and_hdf/scale_csv_by_n.py
@@ -1,63 +1,82 @@
 # Author: Akira Kudo
 # Created: 2024/03/15
-# Last Updated: 2024/03/15
+# Last Updated: 2024/04/20
 
 import os
 
-import numpy as np
 import pandas as pd
 
-def rescale_csv_by_n(csv_path : str, outdir : str, n : int, new_scorer : str, 
-                     also_do_hdf : bool=True, recalculate : bool=False):
-    print("=============================")
-    df = pd.read_csv(csv_path, index_col=[0,1,2], header=[0,1,2])
-    df = df / 2
-    filesafe_n = str(n).replace('.', 'point')
-    old_scorer = os.path.basename(csv_path).replace('CollectedData_', '').replace('.csv', '')
+def scale_dlc_label_csv_by_n(csv_path : str, n : float, outdir : str, 
+                             new_video_name : str=None, new_name : str=None, 
+                             new_scorer : str=None, also_do_hdf : bool=False):
+    """
+    Scales the numerical values contained in a given dlc label csv by n, outputting it
+    to outdir while setting it a new name (generated if not given).
+    Also can reanem the 'scorer' entries if required, as well as output the 
+    resulting csv into hdf.
 
-    new_csvname = os.path.basename(csv_path).replace(
-                                       old_scorer, new_scorer
-                                           )#.replace(
-                                       #'.csv', f'_rescaled_by_{filesafe_n}.csv'
-                                       #    )
-    rescaled_csv_path = os.path.join(outdir, new_csvname)
-    if not os.path.exists(rescaled_csv_path) or recalculate:
-        df.to_csv(rescaled_csv_path, index=True)
-        # then rename the scorer & filename as index
-        with open(rescaled_csv_path, 'r+') as f:
-            lines = f.readlines()
-            # rewrite the content of csv
-            old_video_name = lines[3].split(',')[1]
-            new_video_name = old_video_name + f'_rescaled_by_{filesafe_n}'
+    :param str csv_path: Path to csv holding dlc data.
+    :param float n: Scaling factor.
+    :param str outdir: Path to target directory for output.
+    :param str new_video_name: Name of videos the label data is indicated to come from,
+    defaults to no change.
+    :param str new_name: New name of generated file, defaults to auto-generation.
+    :param str new_scorer: A new 'scorer' for the scorer entry, defaults to no change.
+    :param bool also_do_hdf: Whether to also output hdf of the same name. Defaults to False.
+    """
+    df = pd.read_csv(csv_path, index_col=[0,1,2], header=[0,1,2])
+    df = df * n
+    # get old_scoere if new_scorer is given
+    if new_scorer is not None:
+        old_scorer = os.path.basename(csv_path).replace('CollectedData_', '').replace('.csv', '')
+    # if new_name is not given, define it
+    if new_name is None:
+        filesafe_n = str(n).replace('.', 'point')
+
+        new_name = os.path.basename(csv_path).replace(
+                                        old_scorer, new_scorer
+                                            ).replace(
+                                        '.csv', f'_rescaled_by_{filesafe_n}.csv'
+                                           )
+    rescaled_csv_path = os.path.join(outdir, new_name)    
+    df.to_csv(rescaled_csv_path, index=True)
+    # then rename the scorer & filename as index
+    with open(rescaled_csv_path, 'r+') as f:
+        lines = f.readlines()
+        # rewrite the content of csv
+        # deal with the scorer 
+        if new_scorer is not None:
             lines[0] = lines[0].replace(old_scorer, new_scorer)
+        # as well as the video name
+        if new_video_name is not None:        
+            old_video_name = lines[3].split(',')[1]
             lines = [l.replace(old_video_name, new_video_name) for l in lines]
-            # save the result 
-            f.seek(0); f.truncate() # delete what was already there
-            f.writelines(lines)
-        print(f'Csv generated at: {rescaled_csv_path}!')
+        # save the result 
+        f.seek(0); f.truncate() # delete what was already there
+        f.writelines(lines)
+    print(f'Csv generated at: {rescaled_csv_path}!')
     
     rescaled_hdf_path = rescaled_csv_path.replace('.csv', '.h5')
-    if also_do_hdf and (not os.path.exists(rescaled_hdf_path) or recalculate):
+    if also_do_hdf:
         new_df = pd.read_csv(rescaled_csv_path, index_col=[0,1,2], header=[0,1,2])
         new_df.to_hdf(rescaled_hdf_path, key="rescaled", index=True)
         print(f"Hdf generated at: {rescaled_hdf_path}!")
+
 
 if __name__ == "__main__":
     N = 0.5
     n_filesafe = str(N).replace('.', 'point')
 
     CSV_FILE = "CollectedData_Judy.csv"
-    CSV_HOLDING_FOLDER_ROOT = "/media/Data/Raymond Lab/Q175-D2Cre Open Field Males/Q175-D2Cre Open Field Males Brown-Judy-2024-01-12/labeled-data/full_dataset_including_belly"
-#"/media/Data/Raymond Lab/Q175-D2Cre Open Field Males/Q175-D2Cre Open Field Males Brown Halfscale-Akira-2024-03-15/labeled-data"
+    CSV_HOLDING_FOLDER_ROOT = r"Z:\Raymond Lab\2 Colour D1 D2 Photometry Project\Akira\RaymondLab\DeepLabCut\video_related\data"
 
-    ABOVE_OUTDIR = "/media/Data/Raymond Lab/Q175-D2Cre Open Field Males/Q175-D2Cre Open Field Males Brown Halfscale-Akira-2024-03-15/labeled-data"
+    OUTDIR = r"Z:\Raymond Lab\2 Colour D1 D2 Photometry Project\Akira\RaymondLab\DeepLabCut\video_related\data\result"
 
-    for d in os.listdir(CSV_HOLDING_FOLDER_ROOT):
-        CSV_PATH = os.path.join(CSV_HOLDING_FOLDER_ROOT, d, CSV_FILE)
-        outdir = os.path.join(ABOVE_OUTDIR, d + f"_rescaled_by_{n_filesafe}")
-        print(f"Processing: {CSV_PATH}")
-        try:
-            rescale_csv_by_n(CSV_PATH, outdir=outdir,
-                             n=N, recalculate=True, new_scorer="Akira")
-        except Exception as e:
-            print(e)
+    CSV_PATH = os.path.join(CSV_HOLDING_FOLDER_ROOT, CSV_FILE)
+
+    scale_dlc_label_csv_by_n(csv_path=CSV_PATH, n=N, 
+                             outdir=OUTDIR,
+                             new_video_name='new_video', 
+                             new_name="CollectedData_Akira.csv", 
+                             new_scorer='Akira', 
+                             also_do_hdf=True)
