d9ac3ae63356e8650f92eb23691d2cfae2ef5dae
AkiraKudo <akirakudo901@gmail.com>
2024-05-15 13:55:54 -0700

    Updated extract_label_and_feature_from_csv to save labeled features into a subdirectory within save_path, making the directory if not already there.

--- BSOID/feature_extraction/extract_label_and_feature_from_csv.py
+++ BSOID/feature_extraction/extract_label_and_feature_from_csv.py
@@ -1,6 +1,6 @@
 # Author: Akira Kudo (Copying A TON from the B-SOiD project)
 # Created: 2024/03/19
-# Last updated: 2024/05/10
+# Last updated: 2024/05/15
 
 # These code files were copied on 2024/03/19 from 
 # Visualize_Feature_Distributions_Per_B_SOID_Bout_Cluster.ipynb and the code blocks
@@ -15,14 +15,17 @@ import joblib
 import numpy as np
 import pandas as pd
 
-from .BSOID_code.adp_filt import adp_filt
-from .BSOID_code.bsoid_extract import bsoid_extract
-from .BSOID_code.bsoid_predict import bsoid_predict
-from .utils import Bodypart, generate_guessed_map_of_feature_to_data_index
+from BSOID_code.adp_filt import adp_filt
+from BSOID_code.bsoid_extract import bsoid_extract
+from BSOID_code.bsoid_predict import bsoid_predict
+from utils import Bodypart, generate_guessed_map_of_feature_to_data_index
 
 FEATURE_SAVING_FOLDERS = "./B-SOID_features/results/features"
 FEATURE_FILE_SUFFIX = "_features.npy"
 LABEL_FILE_SUFFIX = "_labels.npy"
+
+LABELED_FEATURE_FOLDERNAME = 'lbl_feats'
+LABELED_FEATURE_CSV_SUFFIX = '_labeled_features.csv'
 
 # Extract both labels and features from a csv file
 
@@ -55,19 +58,20 @@ def extract_label_and_feature_from_csv(filepath : str, pose : List[int],
     features. Defaults to FEATURE_SAVING_FOLDERS.
     """
     # first check relevant paths exist
-    if not os.path.exists(filepath): 
+    if not os.path.exists(filepath):
         raise Exception(f"Following file doesn't seem to exist: {filepath}...")
     if not os.path.exists(clf_path): 
         raise Exception(f"Couldn't find classifier: {filepath}...")
     if save_result and not os.path.exists(save_path): 
-            raise Exception(f"Saving folder wasn't found: {save_path}...")
+        raise Exception(f"Saving folder wasn't found: {save_path}...")
     if not recompute and not os.path.exists(load_path): 
-            raise Exception(f"Folder holding data to potentially load wasn't found: {load_path}...")
+        raise Exception(f"Folder holding data to potentially load wasn't found: {load_path}...")
 
     filename = os.path.basename(filepath).replace('.csv', '')
     clfname = os.path.basename(clf_path).replace('_randomforest.sav', '')
     feature_save_filename = clfname + "_" + filename + FEATURE_FILE_SUFFIX
     label_save_filename   = clfname + "_" + filename + LABEL_FILE_SUFFIX
+    lbld_feats_filename   = clfname + "_" + filename + LABELED_FEATURE_CSV_SUFFIX
 
     if not recompute:
         # attempt fetch
@@ -112,38 +116,47 @@ def extract_label_and_feature_from_csv(filepath : str, pose : List[int],
     if save_result:
         print(f"Saving results to: {save_path}")
         try:
-            if not os.path.exists(os.path.join(save_path, feature_save_filename)):
-                np.save(os.path.join(save_path, feature_save_filename), feature)
-            if not os.path.exists(os.path.join(save_path, label_save_filename)):
-                np.save(os.path.join(save_path, label_save_filename), label)
+            # first create the folder to hold labeled features
+            lbld_feats_save_folder = os.path.join(save_path, LABELED_FEATURE_FOLDERNAME)
+            if not os.path.exists(lbld_feats_save_folder):
+                os.mkdir(lbld_feats_save_folder)
+                
+            # save the features
+            feature_save_path = os.path.join(save_path, feature_save_filename)
+            if os.path.exists(feature_save_path):
+                print(f"Overwriting feature: - {feature_save_filename}")
+            np.save(feature_save_path, feature)
             
-            if not os.path.exists(os.path.join(
-                save_path, 
-                feature_save_filename.replace(FEATURE_FILE_SUFFIX, '_labeled_features.csv')
-                )):
-                # specify which bodypart we consider - predicted from pose
-                bodyparts = []
-                for pose_val in pose:
-                    bp = Bodypart(pose_val // 3)
-                    if bp not in bodyparts:
-                        bodyparts.append(bp)
-                feature_to_index_map = generate_guessed_map_of_feature_to_data_index(
-                    bodyparts, short=True
-                )
-                # then create column names from those info
-                column_header = ["label"] + ["_tofill_"] * len(feature_to_index_map)
-                for feature_name, feature_idx in feature_to_index_map.items():
-                    column_header[feature_idx + 1] = feature_name
-                # finally populate a dataframe with the given data
-                label = label[:feature.shape[1]] # truncate labels by the number of features
-                # len(labels) > len(features) as artifact of padding - have to double check
-                df_data = np.concatenate((np.expand_dims(label, axis=0), feature), 
-                                        axis=0).T
-                saved_df = pd.DataFrame(data=df_data, columns=column_header)
-                saved_df.to_csv(os.path.join(
-                    save_path, 
-                    feature_save_filename.replace(FEATURE_FILE_SUFFIX, '_labeled_features.csv')
-                    ))
+            # then the label
+            label_save_path = os.path.join(save_path, label_save_filename)
+            if os.path.exists(label_save_path):
+                print(f"Overwriting label: - {label_save_filename}")
+            np.save(label_save_path, label)
+
+            # then save the labeled feature
+            lbld_feats_save_path = os.path.join(lbld_feats_save_folder, lbld_feats_filename)
+            if os.path.exists(lbld_feats_save_path):
+                print(f"Overwriting labeled feature: - {lbld_feats_filename}")
+            # specify which bodypart we consider - predicted from pose
+            bodyparts = [Bodypart(pose_val // 3) 
+                         for pose_val in pose 
+                         if Bodypart(pose_val // 3) not in bodyparts]
+            
+            feature_to_index_map = generate_guessed_map_of_feature_to_data_index(
+                bodyparts, short=True
+            )
+            # then create column names from those info
+            column_header = ["label"] + ["_tofill_"] * len(feature_to_index_map)
+            for feature_name, feature_idx in feature_to_index_map.items():
+                column_header[feature_idx + 1] = feature_name
+            # finally populate a dataframe with the given data
+            label = label[:feature.shape[1]] # truncate labels by the number of features
+            # len(labels) > len(features) as artifact of padding - have to double check
+            df_data = np.concatenate((np.expand_dims(label, axis=0), feature), 
+                                    axis=0).T
+            saved_df = pd.DataFrame(data=df_data, columns=column_header)
+            saved_df.to_csv(lbld_feats_save_path)
+
         except Exception as e:
             print(e)
             print("Moving on...")
@@ -170,25 +183,25 @@ def fetch_precomputed_from_npy(path):
 
 
 def compute_merged_features_from_csv_data(data : np.ndarray, fps : int):
-  """
-  Takes a single data np.ndarray and extracts & computes a merged feature set
-  from it.
-  :param np.ndarray data: The data numpy array from which we extract the
-  features.
-  :param int fps: Fps for the video we are dealing with.
-  """
-  feature = None
-  # compute features from data - features is n lists of 10fps binned features
-  # e.g. with a 40 fps video, we get 4 lists of 10 fps binned feature calculated
-  #      something like [0,4], [1,5], [2,6] and [3,7] created from [0,1,2,3,4,5,6,7]
-  #      where each number is the starting frame of a 10 fps bin
-  premerged_feature = bsoid_extract([data], fps)
-  # so we want to interleave them to get the final feature time series
-  feature = np.empty((premerged_feature[0].shape[0], # each row is a feature
-                            sum(feat.shape[1] for feat in premerged_feature)),  # get total length of all 10 fps data
-                            dtype=premerged_feature[0].dtype)
-  stride = len(premerged_feature) # the same feature vector is entered with a stride of how many features there are
-  # e.g. merging [0,3], [1,4] and [2,5] into [0,1,2,3,4,5] is done with a stride of 3
-  for i, feat in enumerate(premerged_feature):
-      feature[:, i::stride] = feat
-  return feature, premerged_feature
+    """
+    Takes a single data np.ndarray and extracts & computes a merged feature set
+    from it.
+    :param np.ndarray data: The data numpy array from which we extract the
+    features.
+    :param int fps: Fps for the video we are dealing with.
+    """
+    feature = None
+    # compute features from data - features is n lists of 10fps binned features
+    # e.g. with a 40 fps video, we get 4 lists of 10 fps binned feature calculated
+    #      something like [0,4], [1,5], [2,6] and [3,7] created from [0,1,2,3,4,5,6,7]
+    #      where each number is the starting frame of a 10 fps bin
+    premerged_feature = bsoid_extract([data], fps)
+    # so we want to interleave them to get the final feature time series
+    feature = np.empty((premerged_feature[0].shape[0], # each row is a feature
+                        sum(feat.shape[1] for feat in premerged_feature)),  # get total length of all 10 fps data
+                        dtype=premerged_feature[0].dtype)
+    stride = len(premerged_feature) # the same feature vector is entered with a stride of how many features there are
+    # e.g. merging [0,3], [1,4] and [2,5] into [0,1,2,3,4,5] is done with a stride of 3
+    for i, feat in enumerate(premerged_feature):
+        feature[:, i::stride] = feat
+    return feature, premerged_feature
