b8f9a9117cf3717162811e7dddb8c0364a148458
AkiraKudo <akirakudo901@gmail.com>
2024-05-25 14:00:12 -0700

    Implemented compute_time_spent_per_label_per_group_from_csvs and compute_time_spent_per_label_per_group_from_numpy_array, using them effectively in do_analyze_time_spent_per_label.

--- BSOID/do_analyze_time_spent_per_label.py
+++ BSOID/do_analyze_time_spent_per_label.py
@@ -1,59 +1,20 @@
 # Author: Akira Kudo
 # Created: 2024/04/03
-# Last Updated: 2024/04/04
+# Last Updated: 2024/05/25
 
 import os
 
 import numpy as np
 import pandas as pd
+import tqdm
 
-from bsoid_io.utils import read_BSOID_labeled_csv
-from feature_analysis_and_visualization.analysis.analyze_time_spent_per_label import analyze_time_spent_per_label_between_groups, compute_time_spent_per_label
+from bsoid_io.utils import read_BSOID_labeled_csv, read_BSOID_labeled_features
+from feature_analysis_and_visualization.analysis.analyze_time_spent_per_label import analyze_time_spent_per_label_between_groups, compute_time_spent_per_label, compute_time_spent_per_label_per_group_from_csvs
 from feature_analysis_and_visualization.utils import get_mousename
-
-YAC_FOLDER = r"C:\Users\mashi\Desktop\temp\YAC"
-WT_FOLDER = r"C:\Users\mashi\Desktop\temp\WT"
-
-SAVE_DIR = r"C:\Users\mashi\Desktop\temp"
-
-# CSV_PATH = r"Z:\Raymond Lab\2 Colour D1 D2 Photometry Project\Akira\Leland B-SOID YAC128 Analysis\YAC128\YAC128\CSV files\BSOID\Mar-10-2023labels_pose_40Hz312152_m2DLC_resnet50_WhiteMice_OpenfieldJan19shuffle1_1030000.csv"
-
-yac_csvs = [os.path.join(YAC_FOLDER, file) for file in os.listdir(YAC_FOLDER) 
-            if '.csv' in file]
-wt_csvs  = [os.path.join(WT_FOLDER,  file) for file in os.listdir(WT_FOLDER )
-            if '.csv' in file]
-
-def analyze_and_save_time_spent_per_group():
-
-    all_labels, yac, wt = analyze_time_spent_per_label_between_groups(
-        yac_csvs, wt_csvs, groups_to_check=None)
-
-    framecount_yac, percentage_yac = yac[0], yac[1]
-    framecount_wt, percentage_wt = wt[0], wt[1]
-
-    def print_group_framecounts_and_percentages(group_name, framecounts, percentages):
-        print(f"{group_name} frames per groups:")
-        [print(f"- {lbl}: {count}") for lbl, count in framecounts.items()]
-        print(f"{group_name} percentages per groups:")
-        [print(f"- {lbl}: {perc}") for lbl, perc in percentages.items()]
-
-    # print_group_framecounts_and_percentages("YAC128", framecount_yac, percentage_yac)
-    # print_group_framecounts_and_percentages("WT", framecount_wt, percentage_wt)
-
-    # our_columns = pd.MultiIndex.from_product((['WT', 'YAC128'], ['framecounts', 'percentages']))
-
-    our_columns = pd.Index(['WT framecounts', 'WT percentages', 'YAC128 framecounts', 'YAC128 percentages'])
-
-    index = pd.Index(all_labels, name='BehaviorGroup')
-    data = [[framecount_yac[lbl], percentage_yac[lbl], framecount_wt[lbl], percentage_wt[lbl]]
-            for lbl in all_labels]
-
-    df = pd.DataFrame(data, index=index, columns=our_columns)
-    df.to_csv(r"C:\Users\mashi\Desktop\temp\labels_spent_over_time.csv")
 
 
 def compute_time_spent_per_label_for_list_of_csv_and_save(
-        groups : list, group_names : list, savedir : str, savename : str
+        groups : list, group_names : list, savedir : str, savename : str, show_message : bool=False
         ):
     """
     Computes time spent per label for a list of csvs, and saves them into
@@ -63,6 +24,7 @@ def compute_time_spent_per_label_for_list_of_csv_and_save(
     :param List[str] group_names: List of names of groups, same length as 'groups'.
     :param str savedir: Directory to save the info as csv.
     :param str savename: Name of saved csv file.
+    :param bool show_message: Whether to show message of the process. Defaults to false.
     """
     if len(groups) != len(group_names): 
         raise Exception("groups has to have the same length as group_names...")
@@ -79,16 +41,18 @@ def compute_time_spent_per_label_for_list_of_csv_and_save(
     data, mouse_types = {}, []
     # for every group and its name
     for group, name in zip(groups, group_names):
+        if show_message:
+            print(f"Processing {name}!")
         # extract the labels for the group
         group_labels = [read_BSOID_labeled_csv(csv_path)[0] for csv_path in group]
         mouse_names  = [get_mousename(csv_path) for csv_path in group]
         # for every extracted label
-        for label, mousename in zip(group_labels, mouse_names):
+        for label, mousename in tqdm.tqdm(zip(group_labels, mouse_names)):
             # add the mouse type to the mouse_types array
             mouse_types.append(name)
             # compute the statistics
             unique_labels, framecount, percentage = compute_time_spent_per_label(
-                        label=label, groups_to_check=None)
+                        label=label, label_groups=None)
             # create padded versions so that we can log them in the same dataframe
             framecount_padded = np.zeros(len(all_unique_labels))
             # percentage_padded = np.zeros(len(all_unique_labels))
@@ -120,11 +84,75 @@ def compute_run_lengths_per_label_for_list_of_csv_and_save(
     :param str savename: _description_
     """
 
-# EXECUTION
+if __name__ == "__main__":
+    if False:
+        YAC_FOLDER = r"C:\Users\mashi\Desktop\temp\YAC"
+        WT_FOLDER = r"C:\Users\mashi\Desktop\temp\WT"
 
-groups = [yac_csvs, wt_csvs]
-group_names = ["YAC128", "WT"]
+        SAVE_DIR = r"C:\Users\mashi\Desktop\temp"
 
-compute_time_spent_per_label_for_list_of_csv_and_save(
-    groups=groups, group_names=group_names,
-    savedir=SAVE_DIR, savename="time_spent_per_label_per_file_YAC128_WT.csv")
+        yac_csvs = [os.path.join(YAC_FOLDER, file) for file in os.listdir(YAC_FOLDER) 
+                    if '.csv' in file]
+        wt_csvs  = [os.path.join(WT_FOLDER,  file) for file in os.listdir(WT_FOLDER )
+                    if '.csv' in file]
+
+        def analyze_and_save_time_spent_per_group():
+
+            all_labels, yac, wt = analyze_time_spent_per_label_between_groups(
+                yac_csvs, wt_csvs, label_groups=None)
+
+            framecount_yac, percentage_yac = yac[0], yac[1]
+            framecount_wt, percentage_wt = wt[0], wt[1]
+
+            def print_group_framecounts_and_percentages(group_name, framecounts, percentages):
+                print(f"{group_name} frames per groups:")
+                [print(f"- {lbl}: {count}") for lbl, count in framecounts.items()]
+                print(f"{group_name} percentages per groups:")
+                [print(f"- {lbl}: {perc}") for lbl, perc in percentages.items()]
+
+            # print_group_framecounts_and_percentages("YAC128", framecount_yac, percentage_yac)
+            # print_group_framecounts_and_percentages("WT", framecount_wt, percentage_wt)
+
+            # our_columns = pd.MultiIndex.from_product((['WT', 'YAC128'], ['framecounts', 'percentages']))
+
+            our_columns = pd.Index(['WT framecounts', 'WT percentages', 'YAC128 framecounts', 'YAC128 percentages'])
+
+            index = pd.Index(all_labels, name='BehaviorGroup')
+            data = [[framecount_yac[lbl], percentage_yac[lbl], framecount_wt[lbl], percentage_wt[lbl]]
+                    for lbl in all_labels]
+
+            df = pd.DataFrame(data, index=index, columns=our_columns)
+            df.to_csv(r"C:\Users\mashi\Desktop\temp\labels_spent_over_time.csv")
+        
+        groups = [yac_csvs, wt_csvs]
+        group_names = ["YAC128", "WT"]
+
+        compute_time_spent_per_label_for_list_of_csv_and_save(
+            groups=groups, group_names=group_names,
+            savedir=SAVE_DIR, savename="time_spent_per_label_per_file_YAC128_WT.csv")
+
+    if True:
+        import os
+        Q175_FOLDER = r"X:\Raymond Lab\2 Colour D1 D2 Photometry Project\Akira\BSOID\Q175\labeled_features\allcsv_2024_05_16_Akira"
+        YAC_FOLDER = r"X:\Raymond Lab\2 Colour D1 D2 Photometry Project\Akira\BSOID\YAC128\labeled_features\allcsv_2024_05_16_Akira"
+        CSV_FOLDER_LIST = [
+            os.path.join(Q175_FOLDER, "HD_filt"),
+            os.path.join(Q175_FOLDER, "WT_filt"),
+            os.path.join(YAC_FOLDER, "HD_filt"),
+            os.path.join(YAC_FOLDER, "WT_filt"),
+            ]
+        GROUPNAMES = ["Q175", "B6", "YAC128", "FVB"]
+
+        SAVE_PATH = r"X:\Raymond Lab\2 Colour D1 D2 Photometry Project\Akira\BSOID\time_spent_per_label_all_micetype_filt.csv"
+
+        CSV_PATH_LIST = [
+            [os.path.join(folder, csv) for csv in os.listdir(folder)] 
+             for folder in CSV_FOLDER_LIST
+        ]
+
+        compute_time_spent_per_label_per_group_from_csvs(
+            csv_groups=CSV_PATH_LIST,
+            group_names=GROUPNAMES,
+            save_path=SAVE_PATH,
+            label_groups=None,
+            save_csv=True)
--- BSOID/feature_analysis_and_visualization/analysis/analyze_time_spent_per_label.py
+++ BSOID/feature_analysis_and_visualization/analysis/analyze_time_spent_per_label.py
@@ -1,47 +1,169 @@
 # Author: Akira Kudo
 # Created: 2024/04/02
-# Last Updated: 2024/04/15
+# Last Updated: 2024/05/25
 
 from typing import List
 
 import numpy as np
+import pandas as pd
+import tqdm
 
-from bsoid_io.utils import read_BSOID_labeled_csv
+from bsoid_io.utils import read_BSOID_labeled_csv, read_BSOID_labeled_features
+from feature_analysis_and_visualization.utils import get_mousename
 
 def compute_time_spent_per_label(label : np.ndarray, 
-                                 groups_to_check : List[int]=None
-                                 ):
+                                 label_groups : List[int]=None):
     """
     Analyzes the number of frames-spent-per-behavior-label in a given
     label numpy array, and returns the result in: 
-    - original frames spent in each groups
-    - percentage of frames spent in each groups
+    - list of unique labels we examined
+    - number of frames spent in each groups in the same order
+    - percentage of time spent in each groups in the same order
 
     :param np.ndarray label: Label extracted from a csv.
-    :param List[int] groups_to_check: A list of labels (integers) specifying
+    :param List[int] label_groups: A list of labels (integers) specifying
     which groups to consider for analysis. Defaults to None - every label.
+
+    :returns np.ndarray unique_labels: List of unique labels we examined.
+    :returns np.ndarray raw_frame_counts: Number of frames spent in each groups in the same order.
+    :returns np.ndarray percentage: Percentage of time spent in each groups in the same order.
     """
-    unique_groups = np.sort(np.unique(label)).astype(np.int64)
+    unique_labels = np.sort(np.unique(label)).astype(np.int64)
     # depending on groups_to_check, filter which group label we consider
-    if groups_to_check is not None and len(groups_to_check) != 0:
-        keep_these_indices = np.isin(unique_groups, np.array(groups_to_check))
-        unique_groups = unique_groups[keep_these_indices]
+    if label_groups is not None and len(label_groups) != 0:
+        keep_these_indices = np.isin(unique_labels, np.array(label_groups))
+        unique_labels = unique_labels[keep_these_indices]
+        unique_labels.sort()
+
+    if len(unique_labels) == 0: 
+        return np.array([]), np.array([]), np.array([])
+    
     # compute the values of interest
     raw_frame_counts = []
-    for unique_lbl in unique_groups:
+    for unique_lbl in unique_labels:
         count = np.sum(label == unique_lbl)
         raw_frame_counts.append(count)
 
     raw_frame_counts = np.array(raw_frame_counts).astype(np.int64)
     percentage = raw_frame_counts / np.sum(raw_frame_counts) * 100
     
-    return unique_groups, raw_frame_counts, percentage
+    return unique_labels, raw_frame_counts, percentage
 
+def compute_time_spent_per_label_per_group_from_csvs(
+        csv_groups : List[List[str]],
+        group_names : List[str],
+        save_path : str,
+        label_groups : List[int]=None,
+        save_csv : bool=True,
+        show_message : bool=False):
+    """
+    Computes the time spent per label in a given DLC csv, for each group 
+    of csvs passed. Then save the result into a csv, indicating:
+    - the number of frames spent by a given mouse in a given label group
+    - the framecount converted into a percentage spent in a given label group
+    - the name of the 'group' the mouse belongs to (useful for marking genotype)
+
+    :param List[List[str]] csv_groups: List of 'groups of csvs', each being a 
+    distinct 'group' of csv paths that hold BSOID mouse data.
+    :param List[str] group_names: List of the name of the corresponding groups.
+    :param str save_path: Path to which we save the csv, including the name of the csv.
+    :param List[int] label_groups: List of integer indicating which label groups
+    to consider when storing their time spent, defaults to all labels.
+    :param bool save_csv: Whether to save the csv, defaults to true.
+    :param bool show_message: Whether to show message of the process. Defaults to false.
+    """
+    nparray_groups = [[read_BSOID_labeled_csv(csv)[0] for csv in group] for group in csv_groups]
+    mousenames = [[get_mousename(csv) for csv in group] for group in csv_groups]
+    df = compute_time_spent_per_label_per_group_from_numpy_array(
+        nparray_groups=nparray_groups,
+        mousenames=mousenames,
+        group_names=group_names,
+        save_path=save_path,
+        label_groups=label_groups,
+        save_csv=save_csv,
+        show_message=show_message)
+    return df
+
+def compute_time_spent_per_label_per_group_from_numpy_array(
+        nparray_groups : List[List[np.ndarray]],
+        mousenames : List[List[str]],
+        group_names : List[str],
+        save_path : str,
+        label_groups : List[int]=None,
+        save_csv : bool=True, 
+        show_message : bool=True):
+    """
+    Computes the time spent per label in a given DLC label np.array, for each group 
+    of np.array passed. Then save the result into a csv, indicating:
+    - the number of frames spent by a given mouse in a given label group
+    - the framecount converted into a percentage spent in a given label group
+    - the name of the 'group' the mouse belongs to (useful for marking genotype)
+
+    :param List[List[str]] nparray_groups: List of 'groups of numpy array', each 
+    being a distinct 'group' of np.ndarray that hold mouse openfield label data.
+    :param List[List[str]] mousenames: List of groups of names of mice, each
+    corresponding to the numpy array contained in nparray_groups. Expected to be
+    the same shape as nparray_groups.
+    :param List[str] group_names: List of the name of the corresponding groups.
+    :param str save_path: Path to which we save the csv, including the name of the csv.
+    :param List[int] label_groups: List of integer indicating which label groups
+    to consider when storing their time spent, defaults to all labels.
+    :param bool save_csv: Whether to save the csv, defaults to true.
+    :param bool show_message: Whether to show message of the process. Defaults to false.
+    """
+    if len(nparray_groups) == 0: return
+    if len(nparray_groups) != len(group_names):
+        raise Exception("Length of nparray_groups and group_names must be the same, but " + 
+                        f"were instead: {len(nparray_groups)} and {len(group_names)}...")
+    if len(nparray_groups) != len(mousenames) or \
+        any([len(nparr) != len(mouse) for nparr, mouse in zip(nparray_groups, mousenames)]):
+        raise Exception("The overall shape of nparray_groups and mousenames must be the same, " + 
+                        "but seem to differ...")
+    
+    data_per_mouse, data_for_df = {}, []
+    # extract the unique labels and their occurrence data first
+    for group, grpname, grp_mousenames in zip(nparray_groups, group_names, mousenames):
+        if show_message:
+            print(f"Processing group: {grpname}!")
+        for nparr, mousename in tqdm.tqdm(zip(group, grp_mousenames)):
+            unique_labels, raw_frame_counts, percentage = compute_time_spent_per_label(
+                label=nparr, label_groups=label_groups
+            )
+            data_per_mouse[mousename] = (unique_labels, raw_frame_counts, percentage, grpname)
+    # then put it together into a pd.DataFrame to save as csv
+    # first identify all the unique labels
+    all_unique_labels = [val[0] for val in data_per_mouse.values()]
+    all_unique_labels = np.unique(np.concatenate(all_unique_labels))
+    # construct rows with the format:
+    # mousename | group1_framecount | group2_count | ... | groupN_count | groupname
+    # mousename | group1_percentage | group2_perct | ... | groupN_perct | groupname
+    for mousename in data_per_mouse.keys():
+        unique_labels, raw_frame_counts, percentage, grpname = data_per_mouse[mousename]
+        # get the sorted framecount & percentage entries into all-zero arrays
+        indices = np.isin(element=all_unique_labels, test_elements=unique_labels)
+        framecount_in_row, percentage_in_row = np.zeros_like(all_unique_labels), np.zeros_like(all_unique_labels)
+        framecount_in_row[indices] = raw_frame_counts
+        percentage_in_row[indices] = percentage
+        # form the rows
+        framecount_row = [mousename] + framecount_in_row.tolist() + [grpname]
+        percentage_row = [mousename] + percentage_in_row.tolist() + [grpname]
+        # insert the rows into the data
+        data_for_df.append(framecount_row); data_for_df.append(percentage_row)
+
+    # create the dataframe
+    df = pd.DataFrame(data=data_for_df, 
+                      columns=["mousename"]+[f"group{lbl}" for lbl in all_unique_labels] + \
+                              ["groupname"],
+                      index=pd.MultiIndex.from_product((list(data_per_mouse.keys()), 
+                                                        ['framecount', 'percentage'])))
+    if save_csv:
+        df.to_csv(save_path)
+    return df
 
 def analyze_time_spent_per_label_between_groups(
         group1 : List[str],
         group2 : List[str],
-        groups_to_check : List[int]=None,
+        label_groups : List[int]=None,
         ):
     """
     Run analysis on aggregated data of groups, passed as lists to 
@@ -54,7 +176,7 @@ def analyze_time_spent_per_label_between_groups(
 
     :param List[str] group1: The first group as list of csv paths.
     :param List[str] group2: The second group as list of csv paths.
-    :param List[int] groups_to_check: A list of all group labels to 
+    :param List[int] label_groups: A list of all group labels to 
     examine for comparion. Defaults to None - all labels.
     :returns List[int] all_labels: All unique labels, sorted ascending.
     :returns (total_framcounts1, avg_percentages1): Both dictionaries 
@@ -66,7 +188,7 @@ def analyze_time_spent_per_label_between_groups(
         framecounts, percentages = {},{}
         for lbl in label_array:
             unique_labels, framecount, percentage = compute_time_spent_per_label(
-                label=lbl, groups_to_check=groups_to_check)
+                label=lbl, label_groups=label_groups)
             for idx, unique_l in enumerate(unique_labels):
                 fc, perc = framecount[idx].item(), percentage[idx].item()
                 framecounts[unique_l] = framecounts.get(unique_l, 0) + fc
@@ -98,15 +220,39 @@ def analyze_time_spent_per_label_between_groups(
 
 
 if __name__ == "__main__":
-    # sanity check: compute_time_spent_per_label
-    label = np.array([0]*25 + [1]*32 + [2]*34 + [0]*100)
-    groups = [[0,1,2],
-              [0,2], 
-              [],
-              [1],
-              [2,1,0]]
+    if False:
+        # sanity check: compute_time_spent_per_label
+        label = np.array([0]*25 + [1]*32 + [2]*34 + [0]*100)
+        groups = [[0,1,2],
+                [0,2], 
+                [],
+                [1],
+                [2,1,0]]
 
-    for g in groups:
-        u_groups, framecounts, percentage = compute_time_spent_per_label(label=label, groups_to_check=g)
-        print(f"g: {g}, u_groups: {u_groups}")
-        print(f"framecounts: {framecounts}, percentage: {percentage}")
+        for g in groups:
+            u_groups, framecounts, percentage = compute_time_spent_per_label(label=label, label_groups=g)
+            print(f"g: {g}, u_groups: {u_groups}")
+            print(f"framecounts: {framecounts}, percentage: {percentage}")
+    
+    if True:
+        import os
+        Q175_FOLDER = r"Z:\Raymond Lab\2 Colour D1 D2 Photometry Project\Akira\BSOID\Q175\labeled_features\allcsv_2024_05_16_Akira"
+        YAC_FOLDER = r"Z:\Raymond Lab\2 Colour D1 D2 Photometry Project\Akira\BSOID\YAC128\labeled_features\allcsv_2024_05_16_Akira"
+        CSV_FOLDER_LIST = [
+            os.path.join(Q175_FOLDER, "HD_filt"),
+            os.path.join(Q175_FOLDER, "WT_filt"),
+            os.path.join(YAC_FOLDER, "HD_filt"),
+            os.path.join(YAC_FOLDER, "WT_filt"),
+            ]
+        GROUPNAMES = ["Q175", "B6", "YAC128", "FVB"]
+
+        CSV_PATH_LIST = [
+            [os.path.join(folder, csv) for csv in os.listdir(folder)] for folder in CSV_FOLDER_LIST
+        ]
+
+        compute_time_spent_per_label_per_group_from_csvs(
+            csv_groups=CSV_PATH_LIST,
+            group_names=GROUPNAMES,
+            save_path=None,
+            label_groups=None,
+            save_csv=False)
