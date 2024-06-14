0aac870cc4eb1c5596aa773856e0364f696641d8
AkiraKudo <akirakudo901@gmail.com>
2024-06-14 10:27:19 -0700

    Implemented visualize_data_in_scatter_dot_plot_from_csv and visualize_data_in_scatter_dot_plot_from_dataframe.

--- DeepLabCut/visualization/visualize_data_in_scatter_dot_plot.py
+++ DeepLabCut/visualization/visualize_data_in_scatter_dot_plot.py
@@ -1,6 +1,6 @@
 # Author: Akira Kudo
 # Created: 2024/04/26
-# Last Updated: 2024/05/30
+# Last Updated: 2024/06/14
 
 import os
 
@@ -19,20 +19,20 @@ CENTER_TIME = 'centerTime'
 TIME_FRACTION_BY_QUADRANT = 'timeFractionByQuadrant'
 DISTANCE_BY_INTERVALS = 'distanceByIntervals'
 
-def visualize_data_in_scatter_dot_plot(csv_path : str,
-                                        ylabel : str,
-                                        y_val : str,
-                                        colors : list,
-                                        save_dir : str,
-                                        save_name : str,
-                                        sex_marker : list=['o', 'o'],
-                                        x_val : str=MOUSETYPE,
-                                        xlabel : str="Mouse Type",
-                                        title : str=None,
-                                        show_median : bool=False,
-                                        show_mean : bool=False,
-                                        save_figure : bool=True,
-                                        show_figure : bool=True):
+def visualize_data_in_scatter_dot_plot_from_csv(csv_path : str,
+                                                ylabel : str,
+                                                y_val : str,
+                                                colors : list,
+                                                save_dir : str,
+                                                save_name : str,
+                                                sex_marker : list=['o', 'o'],
+                                                x_val : str=MOUSETYPE,
+                                                xlabel : str="Mouse Type",
+                                                title : str=None,
+                                                show_median : bool=False,
+                                                show_mean : bool=False,
+                                                save_figure : bool=True,
+                                                show_figure : bool=True):
     """
     Takes in a data csv holding information obtained via running analysis.m
     onto multiple DLC csvs; and holding basic analysis info of open field
@@ -60,9 +60,56 @@ def visualize_data_in_scatter_dot_plot(csv_path : str,
     :param bool save_figure: Whether to save figure, defaults to True.
     :param bool show_figure: Whether to show figure, defaults to True.
     """
-    sex_marker_to_label = {'male' : sex_marker[0], 'female' : sex_marker[1]}
-
     df = pd.read_csv(csv_path)
+    visualize_data_in_scatter_dot_plot_from_dataframe(
+        df=df, ylabel=ylabel, y_val=y_val, colors=colors, save_dir=save_dir, 
+        save_name=save_name, sex_marker=sex_marker, x_val=x_val, xlabel=xlabel,
+        title=title, show_median=show_median, show_mean=show_mean, 
+        save_figure=save_figure, show_figure=show_figure
+        )
+
+def visualize_data_in_scatter_dot_plot_from_dataframe(df : pd.DataFrame,
+                                                    ylabel : str,
+                                                    y_val : str,
+                                                    colors : list,
+                                                    save_dir : str,
+                                                    save_name : str,
+                                                    sex_marker : list=['o', 'o'],
+                                                    x_val : str=MOUSETYPE,
+                                                    xlabel : str="Mouse Type",
+                                                    title : str=None,
+                                                    show_median : bool=False,
+                                                    show_mean : bool=False,
+                                                    save_figure : bool=True,
+                                                    show_figure : bool=True):
+    """
+    Takes in a data frame holding information obtained via running analysis.m
+    onto multiple DLC csvs; and holding basic analysis info of open field
+    mouse activity, such as mouseType, totalDistanceCm, centerTime etc.
+
+    Plots the specified feature, aggregated by mouseType into a scatter plot.
+
+    :param pd.DataFrame df: Data frame holding DLC data.
+    :param str ylabel: Label for y-axis.
+    :param str y_val: Specifies what is rendered as y-label - has to be one 
+    of columns in the DLC csv.
+    :param list colors: A list of colors matching each x entries. If only one 
+    is given, every column is of the same color.
+    :param str save_dir: Directory for saving figure.
+    :param str save_name: Name of saved figure.
+    :param list sex_marker: Marker of ['male', 'female'] mice in the dataset to be 
+    rendered on the scatter plot. A list of MarkerStyle, as shown [here](https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.scatter.html). 
+    Defaults to ['o', 'o'].
+    :param str x_val: Specifies what is rendered as x-label - has to be one
+    of columns in the DLC csv. Defaults to MOUSETYPE.
+    :param str xlabel: Label for x-axis, defaults to "Mouse Type".
+    :param str title: Figure title, defaults to '{xlabel} per {ylabel}'.
+    :param bool show_median: Whether to show median on graph, defaults to False.
+    :param bool show_mean: Whether to show mean on graph, defaults to False.
+    :param bool save_figure: Whether to save figure, defaults to True.
+    :param bool show_figure: Whether to show figure, defaults to True.
+    """
+    sex_marker_to_label = {'male' : sex_marker[0], 'female' : sex_marker[1]}
 
     unique_mousetypes = np.unique(df[MOUSETYPE])
     # make sure enough colors are specified unambiguously
@@ -129,109 +176,6 @@ def visualize_data_in_scatter_dot_plot(csv_path : str,
     else:
         plt.close()
 
-def visualize_time_data_in_dot_and_whisker_plot(
-        csv_path : str,
-        ylabel : str,
-        y_val : str,
-        colors : list,
-        save_dir : str,
-        save_name : str,
-        x_val : str=MOUSETYPE,
-        xlabel : str="Mouse Type",
-        title : str=None,
-        show_median : bool=False,
-        show_mean : bool=False,
-        save_figure : bool=True,
-        show_figure : bool=True
-        ):
-    """
-    Takes in a data csv holding information obtained via running analysis.m
-    onto multiple DLC csvs; and holding basic analysis info of open field
-    mouse activity, such as mouseType, totalDistanceCm, centerTime etc.
-
-    Plots the specified feature, aggregated by mouseType into dot and whisker plot.
-
-    :param str csv_path: Path to csv holding DLC data.
-    :param str ylabel: Label for y-axis.
-    :param str y_val: Specifies what is rendered as y-label - has to be one 
-    of columns in the DLC csv.
-    :param list colors: A list of colors matching each x entries. If only one 
-    is given, every column is of the same color.
-    :param str save_dir: Directory for saving figure.
-    :param str save_name: Name of saved figure.
-    :param str x_val: Specifies what is rendered as x-label - has to be one
-    of columns in the DLC csv. Defaults to MOUSETYPE.
-    :param str xlabel: Label for x-axis, defaults to "Mouse Type".
-    :param str title: Figure title, defaults to '{xlabel} per {ylabel}'.
-    :param bool show_median: Whether to show median on graph, defaults to False.
-    :param bool show_mean: Whether to show mean on graph, defaults to False.
-    :param bool save_figure: Whether to save figure, defaults to True.
-    :param bool show_figure: Whether to show figure, defaults to True.
-    """
-    df = pd.read_csv(csv_path)
-
-    unique_mousetypes = np.unique(df[MOUSETYPE])
-    # make sure enough colors are specified unambiguously
-    if len(colors) != 1 and len(colors) != len(unique_mousetypes):
-        raise Exception("Given color have to match the number of mouse types in csv...")
-    # otherwise if only one color is specified, set color to the same one
-    if len(colors) == 1:
-        colors = colors * len(unique_mousetypes)
-    # set default title if not given
-    if title is None:
-        title = f'{xlabel} per {ylabel}'
-    
-    _, ax = plt.subplots(figsize=(3,6))
-    for i, mstp in enumerate(unique_mousetypes):
-        X = df[x_val].loc[df[MOUSETYPE] == mstp]
-        Y = df[y_val].loc[df[MOUSETYPE] == mstp]
-        ax.scatter(X, Y, color=colors[i])
-        # set boundaries for mean & median bars
-        barwidth = 1/3/(len(unique_mousetypes) + 1)
-        xmax = (i+1) / (len(unique_mousetypes) + 1) - barwidth
-        xmin = (i+1) / (len(unique_mousetypes) + 1) + barwidth
-        # if show_mean is true, show mean
-        if show_mean:
-            ax.axhline(y=np.mean(Y), xmax=xmax, xmin=xmin,
-                       color=colors[i], linestyle=MEAN_LINESTYPE)
-        # if show_median is true, show median
-        if show_median:
-            ax.axhline(y=np.median(Y), xmax=xmax, xmin=xmin,
-                       color=colors[i], linestyle=MEDIAN_LINESTYPE)
-    
-    # set the legend manually for median and mean
-    median_line = mlines.Line2D([],[], color='black', 
-                                linestyle=MEDIAN_LINESTYPE, label='Median')
-    mean_line   = mlines.Line2D([],[], color='black', 
-                                linestyle=MEAN_LINESTYPE,   label='Mean')
-    plt.legend(handles=[median_line, mean_line])
-    # set the other settingss
-    plt.xlabel(xlabel); plt.ylabel(ylabel)
-    plt.suptitle(title)
-    plt.xticks(range(-1, len(unique_mousetypes)+1), rotation=45)
-    plt.tight_layout()
-
-    if save_figure:
-        plt.savefig(os.path.join(save_dir, save_name))
-    
-    if show_figure:
-        plt.show()
-    else:
-        plt.close()
-
-
-    # # rename column to more descriptive name
-    # df.rename(columns={TIME_FRACTION_BY_QUADRANT : f"{TIME_FRACTION_BY_QUADRANT}1",
-    #                    "Unnamed: 5": f"{TIME_FRACTION_BY_QUADRANT}2",
-    #                    "Unnamed: 6": f"{TIME_FRACTION_BY_QUADRANT}3",
-    #                    "Unnamed: 7": f"{TIME_FRACTION_BY_QUADRANT}4", 
-    #                    DISTANCE_BY_INTERVALS : f"{DISTANCE_BY_INTERVALS}1", 
-    #                    "Unnamed: 9": f"{DISTANCE_BY_INTERVALS}2",
-    #                    "Unnamed: 10": f"{DISTANCE_BY_INTERVALS}3",
-    #                    "Unnamed: 11": f"{DISTANCE_BY_INTERVALS}4",
-    #                    "Unnamed: 12": f"{DISTANCE_BY_INTERVALS}5", 
-    #                    "Unnamed: 13": f"{DISTANCE_BY_INTERVALS}6", 
-    #                    }, inplace=True)
     
 if __name__ == "__main__":
     SAVE_DIR = r"Z:\Raymond Lab\2 Colour D1 D2 Photometry Project\Akira\RaymondLab\DeepLabCut\COMPUTED"
@@ -253,7 +197,7 @@ if __name__ == "__main__":
     # DISTANCE_BY_INTERVALS : 'Distance By Intervals (cm)'
     
     for visualized_var, var_name, fig_name in to_visualize:
-        visualize_data_in_scatter_dot_plot(CSVFILE,
+        visualize_data_in_scatter_dot_plot_from_csv(CSVFILE,
                                             y_val=visualized_var,
                                             xlabel=f'Mouse Type ({MOUSETYPE})',
                                             ylabel=var_name,
@@ -265,5 +209,3 @@ if __name__ == "__main__":
                                             show_figure=False,
                                             show_mean=True,
                                             show_median=True)
-    
-   
